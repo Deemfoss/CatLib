aecf5aa4534a9275d992d1176ec0a4da3584e971
artsiom <artsiom@HOME-PC>
2023-01-20 04:43:33 +0300

    update news

--- CatLib/Migrations/20230120014247_add_tags_to_news.Designer.cs
+++ CatLib/Migrations/20230120014247_add_tags_to_news.Designer.cs
@@ -4,14 +4,16 @@ using CatLib.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CatLib.Migrations
 {
     [DbContext(typeof(CatLibContext))]
-    partial class CatLibContextModelSnapshot : ModelSnapshot
+    [Migration("20230120014247_add_tags_to_news")]
+    partial class add_tags_to_news
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
